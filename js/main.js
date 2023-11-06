3a0fe731bf157569e29b530ae41fd6101daeaa2c
JohnQuimson <john.quimson777@gmail.com>
2023-11-06 15:24:45 +0100

    feat: richiesta informazioni

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//DICHIARAZIONI
+const kilometri = parseInt(prompt('Distanza in km: '));
+console.log(kilometri);
+
+const eta = parseInt(prompt('Età del passeggero: '));
+console.log(eta);
