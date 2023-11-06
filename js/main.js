62bec02696499bdd4468c925b65992701134ce3f
JohnQuimson <john.quimson777@gmail.com>
2023-11-06 15:36:08 +0100

    feat: prezzo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
 //DICHIARAZIONI
+const prezzoKilometro = 0.21;
+
 const kilometri = parseInt(prompt('Distanza in km: '));
 console.log(kilometri);
 
 const eta = parseInt(prompt('Età del passeggero: '));
 console.log(eta);
+
+//Calcolo prezzo
+const prezzo = prezzoKilometro * kilometri;
+console.log('Prezzo: ' + prezzo + '€');
