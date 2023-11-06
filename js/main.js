cf4a9ba1b1defdcf53100cc03d8d187d9027c1b7
JohnQuimson <john.quimson777@gmail.com>
2023-11-06 16:22:16 +0100

    feat: condizioni js & animazione treno

--- js/main.js
+++ js/main.js
@@ -2,13 +2,26 @@
 
 //DICHIARAZIONI
 const prezzoKilometro = 0.21;
+const scontoMinorenni = 0.8;
+const scontoOver65 = 0.6;
 
 const kilometri = parseInt(prompt('Distanza in km: '));
-console.log(kilometri);
+console.log(kilometri + ' km');
 
 const eta = parseInt(prompt('Età del passeggero: '));
-console.log(eta);
+console.log(eta + ' anni');
 
 //Calcolo prezzo
-const prezzo = prezzoKilometro * kilometri;
-console.log('Prezzo: ' + prezzo + '€');
+let prezzo = prezzoKilometro * kilometri;
+console.log('Prezzo biglietto: ' + prezzo.toFixed(2) + '€');
+
+//CONDIZIONI
+if (eta < 18) {
+  prezzo = prezzo * scontoMinorenni;
+  console.log('Prezzo con sconto minorenni: ' + prezzo.toFixed(2) + '€');
+} else if (eta >= 65) {
+  prezzo = prezzo * scontoOver65;
+  console.log('Prezzo con sconto Over 65: ' + prezzo.toFixed(2) + '€');
+} else {
+  console.log('Non vengono applicati sconti.');
+}
